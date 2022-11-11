984d5da5ded9e8d856bba0aac240756c27b6a494
lucas <lucasmastro93@gmail.com>
2022-11-11 12:57:25 -0300

    arregle un link

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Vol from './Vol';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+   <>
+   <Navbar/>
+    <Vol/>
+   </>
+  );
+}
+
+export default App;
